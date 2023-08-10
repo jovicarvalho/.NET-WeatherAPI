d293e69783a5fd44a8de00e91f37a42deb23a58c
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-08-09 23:52:17 -0300

    feat: post/get controller + migrations + db configuration

---
+++ WeatherAPI-DOTNET/Controllers/MeteorologicalDataController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAPI_DOTNET.Context;
+using WeatherAPI_DOTNET.Models;
+
+namespace WeatherAPI_DOTNET.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MeteorologicalDataController: ControllerBase
+{
+    private MeteorologicalDataContext _context;
+    public MeteorologicalDataController(MeteorologicalDataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public IActionResult CreateMeteorologicalData([FromBody] MeteorologicalDataEntity metData)
+    {
+        _context.MeteologicalData.Add(metData);
+        _context.SaveChanges();
+        return Ok();
+    }
+
+    [HttpGet]
+    public IEnumerable<MeteorologicalDataEntity> GetAll([FromQuery] int skip = 0)
+    {
+        return _context.MeteologicalData.Skip(0).Take(10);
+    }
+}
---
+++ WeatherAPI-DOTNET/Data/MeteorologicalDataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeatherAPI_DOTNET.Models;
+
+namespace WeatherAPI_DOTNET.Context;
+
+public class MeteorologicalDataContext: DbContext
+{
+    public MeteorologicalDataContext(DbContextOptions<MeteorologicalDataContext> opts): base(opts) { 
+    }
+    public DbSet<MeteorologicalDataEntity> MeteologicalData { get; set; }
+}
