65eab6bc1e667574422a9dd323dc8c706044b61c
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-08-11 02:02:16 -0300

    feat:Creating Edit by Id and Parcial Edit by ID

---
+++ WeatherAPI-DOTNET/Data/Dtos/UpdateMetDataDto.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeatherAPI_DOTNET.Data.Dtos;
+
+public class UpdateMetDataDto 
+{
+    [Required(ErrorMessage = "Informar a cidade é obrigatório.")]
+    [StringLength(35, ErrorMessage = "O nome da cidade não deve exceder 50 caracteres")]
+    public string City { get; set; }
+
+    [Required(ErrorMessage = "Campo de data é obrigatório.")]
+    public DateTime WeatherDate { get; set; }
+
+    [Required(ErrorMessage = "Informar o clima da manhã é obrigatório")]
+    public string MorningWeather { get; set; }
+
+    [Required(ErrorMessage = "Informar o clima da noite é obrigatório")]
+    public string NightWeather { get; set; }
+
+    [Required(ErrorMessage = "O usuário deve informar a temperatura máxima do dia.")]
+    public int MaxTemperature { get; set; }
+
+    [Required(ErrorMessage = "O usuário deve informar a temperatura mínima do dia")]
+    public int MinTemperature { get; set; }
+
+    [Required(ErrorMessage = "O campo de humidade é obrigatório")]
+    [Range(0, 100, ErrorMessage = "A humidade deve ser entre 0 e 100%")]
+    public int humidity { get; set; }
+
+    [Required(ErrorMessage = "O campo de precipitação é obrigatório")]
+    [Range(0, 100, ErrorMessage = "A precipitação deve ser entre 0 e 100%")]
+    public int Precipitation { get; set; }
+
+    [Required(ErrorMessage = "O campo de humidade é obrigatório")]
+    [Range(0, 200, ErrorMessage = "A velocidade do vento deve ser maior que 0 e menor que 200 km/h")]
+    public int WindSpeed { get; set; }
+
+}
