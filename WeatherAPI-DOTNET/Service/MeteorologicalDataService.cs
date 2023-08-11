40eb5156ded8e4d86894a1a8dc9caf11a263abaa
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-08-11 09:06:57 -0300

    feat: service layout for MetData

---
+++ WeatherAPI-DOTNET/Service/MeteorologicalDataService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WeatherAPI_DOTNET.Context;
+
+namespace WeatherAPI_DOTNET.Service;
+
+public class MeteorologicalDataService
+{
+    private MeteorologicalDataContext _context;
+    private IMapper _mapper;
+    public MeteorologicalDataService(MeteorologicalDataContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+}
