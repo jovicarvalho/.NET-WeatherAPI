246162a15025cfc0281c06f695692d8ea4a16219
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-08-16 21:11:14 -0300

    chore: moving the business rules to service layer

--- WeatherAPI-DOTNET/Service/Interfaces/IMeteorologicalDataService.cs
+++ WeatherAPI-DOTNET/Service/Interfaces/IMeteorologicalDataService.cs
@@ -1,11 +1,20 @@
-﻿using WeatherAPI_DOTNET.Models;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using WeatherAPI_DOTNET.Data.Dtos;
+using WeatherAPI_DOTNET.Models;
 
 namespace WeatherAPI_DOTNET.Service.Interfaces
 {
     public interface IMeteorologicalDataService
     {
-        MeteorologicalDataEntity findMeteorologicalDataByID(int id);
+        MeteorologicalDataEntity FindMeteorologicalDataByID(int id);
         List<MeteorologicalDataEntity> FindMeteorologicalDataByCityName(string cityName);
-        public MeteorologicalDataEntity FindMeteoroloficalDataBySpecificDate(DateTime dateOnly, List<MeteorologicalDataEntity> metDataList);
+        MeteorologicalDataEntity FindMeteoroloficalDataBySpecificDate(DateTime dateOnly, string cityName);
+        MeteorologicalDataEntity FindActualDay(string cityName);
+        MeteorologicalDataEntity CreateMeteorologicalData(CreateMetDataDto metDataDto);
+        IEnumerable<MeteorologicalDataEntity> FindAllMeteorologicalData(int skip);
+        MeteorologicalDataEntity EditMeteorologicalData(int id, UpdateMetDataDto metDataDto);
+        MeteorologicalDataEntity EditOnlyOneField(int id, JsonPatchDocument<UpdateMetDataDto> patch);
+        MeteorologicalDataEntity DeleteMeteorologicalData(int id);
+
     }
 }
